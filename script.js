645e287b5df7b6368113fe3d21e2cc26a55e5872
Majid Moghadam <majid.moghadam114@yahoo.com>
2025-04-14 14:39:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.querySelectorAll(".sentence").forEach(div => {
       span.classList.add("highlight");
       const tempUtter = new SpeechSynthesisUtterance(span.textContent);
       tempUtter.lang = lang;
-      tempUtter.rate = 3.5;
+      tempUtter.rate = 2;
       speechSynthesis.cancel();
       speechSynthesis.speak(tempUtter);
     });
